df8ae9cc8cbe14cb34dd8bceb2c775db27c94f09
Mhodd <33404472+Mhodd@users.noreply.github.com>
2019-05-09 02:11:08 +0100

    Initialized forager v2, remove previous source code files. Implemented the top level forager controller. Its responsibility is to perform a top level merge on the aggregates returned by each drivers, enabling multiple database foraging.

--- index.js
+++ index.js
@@ -1,103 +1,41 @@
-var _ = require('lodash');
-var async = require('async-chainable');
-var argy = require('argy');
-var debug = require('debug')('forager');
+const driversByDatabase = {
 
-var foragerDefaults = {
-	drivers: {
-		bondLibrary: require('./drivers/bondLibrary'),
-		doi: require('./drivers/doi'),
-		doiDx: require('./drivers/doiDx'),
-		google: require('./drivers/google'),
-		googleScholar: require('./drivers/googleScholar'),
-		wos: require('./drivers/wos'),
-	},
-	wos: { // Put your WoS credentials here if you have any
-		user: '',
-		pass: '',
-	},
-};
+}
 
-function Forager(options) {
-	var forager = {};
-	forager.settings = _.defaults(options, foragerDefaults);
+/**
+ * @param {Object[]} citations
+ * @param {Options} options 
+ * @param {Object[]} drivers
+ * @param {string} drivers.database
+ * @param {Object} drivers.config
+ * @returns {Promise<Object[]>}
+ */
+const forageCitations = async (citations, options) => {
+  const selectedDrivers = options.drivers.map(driver => driversByDatabase[driver.database](driver.config));
 
-	/**
-	* Set an options setting
-	* @param {string|array|Object} setting Either the key to set or an entire object when setting multiple
-	* @param {*} [val] The value if setting is a string / single setting to set
-	*/
-	forager.set = argy('object|string|array [*]', (setting, val) => {
-		if (_.isObject(setting)) {
-			_.merge(forager.settings, setting);
-		} else {
-			_.set(forager.settings setting, val);
-		}
-	});
+  console.log(`Started foraging ${citations.length} citations`);
 
+  const aggregatedCitations = await Promise.all(citations.map(async citation => {
+    const aggregatedCitationByDriver = {};
 
-	/**
-	* Find all URLS for a given reference
-	* The reference should conform to the [RefLib](https://github.com/hash-bang/Reflib) field definitions
-	* @param {Object} ref A reference to find links for, if this is an object its assumed it is a valid RefLib reference, if a string its assumed to be a DOI
-	* @param {Object} options An optional options object to use in addition to this objects own options + the global defaults
-	* @param {function} callback The callback. Will be fired with (err, refURL)
-	*/
-	forager.populate = argy('object|string [object] function', (rawRef, options, callback) => {
-		var ref = _.isString(rawRef) ? {doi: rawRef} : rawRef;
+    await Promise.all(selectedDrivers.map(async driver => {
+      try {
+        aggregatedCitationByDriver[driver.database] = await driver.forageCitation(citation, options);
+      } catch (error) {
+        console.log(`${driver.database}: Something went wrong.`);
+      }
+    }))
 
-		var settings = _.assign(forager.settings, options);
+    return Object.values(aggregatedCitationByDriver).reduce((a, b) => ({ ...a, ...b }));
+  }));
 
-		async()
-			.set('urls', {})
-			.forEach(forager.drivers, function(nextDriver, driver, driverID) {
-				driver.populate(ref, settings, (err, url) => {
-					if (err) {
-						debug('Driver', driverID, 'ERR -', err.toString());
-					} else if (!url) {
-						debug('Driver', driverID, 'recieved empty response');
-					} else if (_.isObject(url)) { // Got multiple urls - merge them
-						_.assign(this.urls, url);
-					} else if (_.isString(url)) {
-						this.urls[driverID] = url;
-					} else {
-						throw new Error('Got URLs back from driver ' + driverID + ' in unknown format: ' + typeof url);
-					}
-					nextDriver();
-				});
-			})
-			.end(function(err) {
-				if (err) return callback(err);
-				callback(null, this.urls);
-			});
-	});
+  console.log(`Finished foraging`);
 
+  return aggregatedCitations;
+}
 
-	/**
-	* Get an object representing all fields that populate() could return
-	* @param {function} callback The callback, which will be fired as (err, object)
-	*/
-	forager.fields = argy('function', function(callback) {
-		async()
-			.set('fields', {})
-			.forEach(forager.drivers, function(nextDriver, driver, driverID) {
-				driver.fields((err, fields) => {
-					if (err) return nextDriver(err);
-					_.assign(this.fields, fields); // Merge with field definitions
-					nextDriver();
-				})
-			})
-			.end(function(err) {
-				callback(err, this.fields);
-			});
-	});
+const forager = {
+  forageCitations,
+}
 
-
-	// Initiate drivers
-	forager.drivers = _.mapValues(forager.settings.drivers, (d, id) => new d(forager));
-
-	return forager;
-};
-
-module.exports = Forager;
-module.exports.defaults = foragerDefaults;
+module.exports = forager;
--- test/settings.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	wos: { // If you have a valid WOS login place it here for testing
-		user: '',
-		pass: '',
-	},
-};
